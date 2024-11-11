9f9441beba09e44fe90ba7f7d4919e98fa44f6d1
Matana P <140636857+beungz@users.noreply.github.com>
2024-11-11 22:28:25 +0700

    Added minor comments on the code

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -4,6 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    """Main HTTP Trigger Function for Azure Functions"""
     logging.info('Python HTTP trigger function processed a request.')
     from FlaskApp import flask_app
     return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
